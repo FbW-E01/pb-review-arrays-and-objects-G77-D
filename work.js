1e0e69da3d982114c1dd644871c1b162b8027920
Gary Dineen <dineen77@hotmail.com>
2021-05-27 14:12:02 +0200

    clean up

--- work.js
+++ work.js
@@ -1,74 +1,72 @@
 /* 1. Create an array that contains three strings.*/
 
-Const [ "BmW", "honda FireBlade", "Yamaha R1"];
+const bike = [ "BmW", "honda FireBlade", "Yamaha R1"];
 
 /* 2. Create an array that contains one string, one number and one boolean.*/
 
-const["May", 25, true];
+const boo = ["May", 25, true];
 
 /*3. Create an array hat contains three empty arrays.*/
-const["", "", ""];
- 
+const empty = [[], [], []];
 /* 4. Create an empty object.*/
 
-const EmptyObject = {};
+const emptyObject = {};
 
 /* 5. Create an object with three properties: id, email, password.*/
 
-const User = {
+const user = {
   id: "Passport",
   email: "Heloo@hotmail.com",
-  Password: "Dontlook",
-  
-}
+  password: "Dontlook",
+};
 
 /* 6. Create an array that contains three objects. All objects should have a width and a height property.*/
 
-const newHexagon [
+const newHexagon = [
   { width: 5, height: 5}, 
   { width: 5, height: 5},
   { width: 5, height: 5},
   { width: 5, height: 5},
   { width: 5, height: 5},
 ]
-  
 
 /*7. Create an object that contains three properties: a string, an array and an object. Invent your own property names and values.*/
 
-Const User = {
-  UserName: "Steve Jobs",
-  Companies:["Apple", "Pinxar", "Next"]
+const user1 = {
+  userName: "Steve Jobs",
+  companies: ["Apple", "Pinxar", "Next"],
   date: { year: 1984, year: 1986, year: 1997, },
 }
 
 /*8. Print the `type` of a variable that has an array value.*/
-console.log(typeof ArraysUser);
+console.log(typeof newHexagon);
 
 /*9. Print the `type` of a variable that has an object value.*/
 
-console.log(typeof Object);
+console.log(typeof user1);
 
 /*10. Create an array with five random values.*/
 
-const Array1 = ["UserName", true, [1, 2, 3, 4] { Football: "Mancity", "Liverpool" }, 5]
+const Array1 = ["UserName", true, [1, 2, 3, 4], { Football: "Mancity", lastplace : "Liverpool" }, 5]
   ;
 
 /*10.1.Add one value to the end of the array.*/
-ArrayTom.push("value add to the end ");
+Array1.push("value add to the end ");
 
 /*10.2.Add one thing to the beginning of the array.*/
-ArrayTom.unshift(" value add to the start");
+Array1.unshift(" value add to the start");
 
 
 /*10.3.Take out one thing from the end of the array.*/
-  ArrayTom.pop();
+  Array1.pop();
 
 /*10.4.Print the length of the array.*/
-  console.log(ArrayTom.length);
+  console.log(Array1.length);
 
-10.5.Take out one thing from the end of the array.
-const removeValueofArray = ArrayTom.pop()
+/*10.5.Take out one thing from the end of the array.*/
+
+const removeValueofArray = Array1.pop()
 
 
-10.6.Take out one the 2nd thing from the array.
-  remove2ndValueofArray = ArrayTom.splice(1,1);
+/*10.6.Take out one the 2nd thing from the array.*/
+  remove2ndValueofArray = Array1.splice(1,1);
